962400ac0d963ef1bc270687fc29f4118289ab39
Landerson Miranda <80685802+landersonmiranda@users.noreply.github.com>
2025-01-09 16:19:17 -0300

    Add files via upload

---
+++ wwwroot/js/site.js
@@ -0,0 +1,65 @@
+﻿// Please see documentation at https://learn.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+
+$(document).ready(function () {
+
+    $('#table-contatos').dataTable({
+        columnDefs: [{
+            "defaultContent": "",
+            "targets": "_all"
+        }],
+    });
+    getDatatable('#table-usuarios');
+    getDatatable('#table-contatos-usuario');
+   
+
+    $('.btn-total-contatos').click(function () {
+        var usuarioId = $(this).attr('usuario-id');
+
+        $.ajax({
+            type:'GET',
+            url: '/Usuario/ListarContatosPorUsuarioId/' + usuarioId, 
+            success: function (result) {
+                $("#listaContatosUsuario").html(result);
+                $('#modalContatosUsuario').modal('show');
+            }
+        });
+    });
+})
+
+function getDatatable(id) {
+    $(id).DataTable({
+        "ordering": true,
+        "paging": true,
+        "searching": true,
+        "oLanguage": {
+            "sEmptyTable": "Nenhum registro encontrado na tabela",
+            "sInfo": "Mostrar _START_ até _END_ de _TOTAL_ registros",
+            "sInfoEmpty": "Mostrar 0 até 0 de 0 Registros",
+            "sInfoFiltered": "(Filtrar de _MAX_ total registros)",
+            "sInfoPostFix": "",
+            "sInfoThousands": ".",
+            "sLengthMenu": "Mostrar _MENU_ registros por pagina",
+            "sLoadingRecords": "Carregando...",
+            "sProcessing": "Processando...",
+            "sZeroRecords": "Nenhum registro encontrado",
+            "sSearch": "Pesquisar",
+            "oPaginate": {
+                "sNext": "Proximo",
+                "sPrevious": "Anterior",
+
+            },
+            "oAria": {
+                "sSortAscending": ": Ordenar colunas de forma ascendente",
+                "sSortDescending": ": Ordenar colunas de forma descendente"
+            }
+        }
+    });
+
+}
+
+$('.close-alert').click(function () {
+    $('.alert').hide('hide');
+});
